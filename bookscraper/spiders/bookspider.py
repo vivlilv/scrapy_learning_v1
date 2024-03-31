97e51d1a93b5e2aa7c4761a161976366f43dc007
vivliv <viv8liv@gmail.com>
2024-03-31 21:57:52 +0300

    part 5 of tutorial. Used XPATH for some "hard-to-get" data; now each book page is scraped and data written in file

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -7,25 +7,46 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+
+        #iterate over books on one page and extract data from their detail page
         books = response.css("article.product_pod")
-
         for book in books:
-            yield{#similiar to return
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a').attrib['href'],
-            }
+            relative_url = book.css('h3 a').attrib['href']
 
-
-        '''
-        Check if the "next page" exists, if yes add its url to response and parse that page also
-        callback means that the function (parse which is called recursively) will be executed after we get a response from that page
-        '''
-        next_page = response.css('li.next a').attrib['href']
-        if next_page is not None:
-            if 'catalogue' in next_page:
-                next_page_url = 'https://books.toscrape.com/' + next_page
+            if 'catalogue' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
 
-            yield response.follow(next_page_url, callback=self.parse)
+            yield response.follow(book_url, callback=self.parse_book_page)
+
+
+        # #iterate over pages created by pagination
+        # next_page = response.css('li.next a').attr['href'].get()
+        # if next_page is not None:
+        #     if 'catalogue' in next_page:
+        #         next_page_url = start_urls[0]+next_page
+        #     else:
+        #         next_page_url = start_urls[0]+'catalogue/'+next_page
+            
+        #     yield response.follow(next_page_url, callback=self.parse)
+
+
+    def parse_book_page(self, response):
+        
+        table_rows = response.css('table tr')
+
+        yield {
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css('td::text').get(),
+            'price_excl_tax': table_rows[2].css('td::text').get(),
+            'price_incl_txt': table_rows[3].css('td::text').get(),
+            'tax': table_rows[4].css('td::text').get(),
+            'availability': table_rows[5].css('td::text').get(),
+            'num_of_reviews': table_rows[6].css('td::text').get(),
+            'stars': response.css('p.star-rating ').attrib['class'].split()[1],
+            'category': response.xpath('//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get(),
+            'description': response.xpath('//div[@class="product_description"]/following-sibling::p/text()').get(),
+            'price':response.css('p.price_color ::text').get(),
+        }
